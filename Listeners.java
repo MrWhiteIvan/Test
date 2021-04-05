4e1d5bbcd973d70c6fc33d5db3ef4f305ff5aaff
m4rsi <71939433+m4rsi@users.noreply.github.com>
2021-04-05 21:36:08 +0300

    Update Listeners.java

--- Listeners.java
+++ Listeners.java
@@ -1,3 +1,13 @@
 public class Listeners implements Listener {
-  
+
+ @EventHandler
+ public void onPlayerJoin(PlayerJoinEvent event){
+   Player player = event.getPlayer();
+   event.setJoinMessage(player.getDisplayName + ChatColor.RED + " join to consolka");
+ }
+
+ public void onPlayerQuit(PlayerQuitEvent event) {
+    Player player = event.getPlayer();
+    event.setQuitMessage(player.getDisplayName() + ChatColor.YELOW + " leave consolka");
+  }
 }
