4e4a714e1e99de70d5922af7624bbd778504b4a4
m4rsi <71939433+m4rsi@users.noreply.github.com>
2021-04-05 21:51:15 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,14 @@
+package ru.hyper.test;
+
 import org.bukkit.plugin.java.JavaPlugin;
+import ru.hyper.test.listeners.Listeners;
 
 public class Main extends JavaPlugin {
     @Override
     public void onEnable() {
      getServer().getPluginManager().registerEvents(new Listeners(), this);
+  }
+    @Override
+    public void onDisable(){
   }
 }
