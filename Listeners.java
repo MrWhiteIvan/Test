fa70aec16bf80c2f0e136ab7252ec4f362ef1c6f
MrWhiteIvan <81985809+MrWhiteIvan@users.noreply.github.com>
2021-04-05 21:53:45 +0300

    Update Listeners.java

--- Listeners.java
+++ Listeners.java
@@ -2,7 +2,7 @@ package ru.hyper.test;
 
 import org.bukkit.ChatColor;
 import org.bukkit.event.Listener;
-import org.bukkit.event.EventHandler
+import org.bukkit.event.EventHandler;
 import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
