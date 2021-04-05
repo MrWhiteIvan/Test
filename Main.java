412d372abea4891bed5d8e35d62dc86a8a633516
m4rsi <71939433+m4rsi@users.noreply.github.com>
2021-04-05 21:57:27 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,6 +5,10 @@ import ru.hyper.test.listeners.Listeners;
 
 public class Main extends JavaPlugin {
     @Override
+    public void onLoad(){
+     getServer().getConsoleSender().sendMessage("Плагин загружается..");
+  }
+    @Override
     public void onEnable() {
      getServer().getPluginManager().registerEvents(new Listeners(), this);
      getServer().getConsoleSender().sendMessage("Плагин успешно включен!");
